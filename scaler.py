e779f30a291d83aaf1773a37eb769631ecf9fe6d
Jan Florian Dietrich <jfd@daenen4.de>
2014-08-15 12:16:22 +0200

    Added validation for widths

--- scaler.py
+++ scaler.py
@@ -1,5 +1,5 @@
 from __future__ import division
-from PIL import Image, ImageOps
+from PIL import Image
 import settings
 
 
--- targets.py
+++ targets.py
@@ -18,21 +18,40 @@ class Target(object):
 
 
 class DefaultTarget(Target):
+    allowed_widths = []
+
     def __init__(self, riak_img, width):
-        self.width = int(width)
         self.riak_img = riak_img
+        self.width = self.validate_widths(width)
         self.scaler = Scaler(self.riak_img, self.width)
 
+    def validate_widths(self, width):
+        if width not in self.allowed_widths and width != '0' and width != 'unscaled':
+            raise ValueError('You requested a width that is not allowed for this target')
+
+        if width == '0' or width == 'unscaled':
+            width = self.riak_img.width
+
+        return int(width)
+
+
 class ScaleTarget(DefaultTarget):
+    allowed_widths = ['200', '400', '600', '800', '1400']
+
     def convert(self):
         return self.scaler.scale()
 
+
 class CropTarget(DefaultTarget):
+    allowed_widths = ['200', '400', '600', '800', '1400']
     aspect_ratio = 16/9
+
     def convert(self):
         return self.scaler.crop(self.aspect_ratio)
 
+
 class LetterboxTarget(DefaultTarget):
+    allowed_widths = ['200', '400', '600', '800', '1400']
     aspect_ratio = 16/9
 
     def convert(self):
